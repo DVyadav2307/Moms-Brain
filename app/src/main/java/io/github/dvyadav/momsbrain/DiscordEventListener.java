b3ebd739592312063e60b3a5eb7181ce6b1102d2
Divyanshu Yadav <divyanshuy858@gmail.com>
2024-06-24 23:13:38 +0530

    temp notes and welcome msg feature added

--- app/src/main/java/io/github/dvyadav/momsbrain/DiscordEventListener.java
+++ app/src/main/java/io/github/dvyadav/momsbrain/DiscordEventListener.java
@@ -1,7 +1,9 @@
 package io.github.dvyadav.momsbrain;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -18,16 +20,38 @@ public class DiscordEventListener extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event){
         if(event.getAuthor().isBot()) return;
 
-        Message msg = event.getMessage()    ;
+        Message msg = event.getMessage();
+
+        if(msg.getContentRaw().equals("!printName")){
+            event.getChannel().sendMessage("Namee of Channel is :"+event.getGuild().getTextChannelById(1252188418817921077L).getName()).queue();
+        }
+
+        if(msg.getContentRaw().equals("!printID")){
+            event.getChannel().sendMessage("Channel "+event.getChannel().getName()+" ID is:"+event.getChannel().getId()).queue();
+        }
+
         if(msg.getContentRaw().toLowerCase().contains("shit")){
             System.out.println("___ "+event.getAuthor()+"SAID THIS"+msg.getContentRaw()+"___");
             msg.delete().queue((v) ->{
                 MessageChannel chnl = event.getChannel();
-                chnl.sendMessage(v+" Mind Your Language "+event.getAuthor().getEffectiveName()).queue(/* (w) ->
+                chnl.sendMessage(" Mind Your Language "+event.getAuthor().getEffectiveName()).queue(/* (w) ->
                 {
                     event.getAuthor().
                 } */);
             });
         }
     }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event){
+
+        String newMembeeName = event.getUser().getAsMention();
+
+        System.out.println("___"+newMembeeName+" JOINED THE "+event.getGuild().getName()+"___");
+
+        TextChannel channel = event.getGuild().getTextChannelById(1252188418817921077L);
+        channel.sendMessage("Welcome to Mom's Basement "+newMembeeName+",\nPlz Check Pinned Messege for the Notes!!").queue();
+        
+    }
+
 }
--- app/src/main/java/io/github/dvyadav/momsbrain/Main.java
+++ app/src/main/java/io/github/dvyadav/momsbrain/Main.java
@@ -1,14 +1,19 @@
 package io.github.dvyadav.momsbrain;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
 public class Main {
     public static void main(String[] args) {
 
-        // // New JDA api from JDA builder
-        // JDA api = JDABuilder.createDefault(System.getenv("MOM_BOT_TOKEN"))
-        //         .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-        //         .build();
-        // api.addEventListener(new DiscordEventListener());
+        // New JDA api from JDA builder
+        JDA api = JDABuilder.createDefault(System.getenv("MOM_BOT_TOKEN"))
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MEMBERS)
+                .build();
+        api.addEventListener(new DiscordEventListener());
 
+        
         
     }
 }
