2dc9520cc1b0dad0339ff1c73bc598a36168b5ad
Divyanshu Yadav <divyanshuy858@gmail.com>
2024-07-02 03:22:17 +0530

    Warn abuser and report to admin

--- app/src/main/java/io/github/dvyadav/momsbrain/DiscordEventListener.java
+++ app/src/main/java/io/github/dvyadav/momsbrain/DiscordEventListener.java
@@ -4,13 +4,18 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Member;
 
 @SuppressWarnings("null")
 public class DiscordEventListener extends ListenerAdapter {
@@ -22,15 +27,14 @@ public class DiscordEventListener extends ListenerAdapter {
     private BufferedReader reader ;
 
     /* object to load the profane words from file to the Set */
-    private Set<String> cusswords = new HashSet<>();
+    private Set<String> cusswordsSet = new HashSet<>();
     
     @Override
     public void onReady(ReadyEvent event){
         System.out.println("___THE BOT IS READY___");
 
         /* cuss-words file read operation on saperate virtual thread */
-        @SuppressWarnings("unused")
-        Thread thread = Thread.ofVirtual().start(() -> {
+        Thread.ofVirtual().start(() -> {
 
             try {
 
@@ -38,7 +42,7 @@ public class DiscordEventListener extends ListenerAdapter {
                 reader = new BufferedReader(new FileReader(new File(CUSSWORDSFILE.toURI())));
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    cusswords.add(line.trim());
+                    cusswordsSet.add(line.trim());
                 }
 
             } catch (Exception e) {
@@ -56,15 +60,42 @@ public class DiscordEventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event){
 
-        if(event.getMessage().getContentRaw().equals("!printCuss")){
+        Message chatMsg = event.getMessage();
 
-            event.getChannel().sendMessage("logging .... Check Logs!!").queue();
+        /* ignore messeges from bots */
+        if(event.getAuthor().isBot()) return;
 
-            for (String string : cusswords) {
-                System.out.println(">>> "+ string);
+        /* Handle cuss chats in saperate thread */
+        Thread.ofVirtual().start(()->{
+
+            String[] words = chatMsg
+            .getContentStripped()                              /* remove text formatting */                          
+            .toLowerCase()                                     /* list is in LowerCase so words should be in LC too */
+            .split("\\s+|,\\s*|\\.\\s*|\\?\\s*|!\\s*");  /* spilt sentence to words acc to regex */
+
+            for (String word : words){ /* check for each word in the sentence */
+                if(cusswordsSet.contains(word)){/* if the word is a cuss */
+                    
+                    chatMsg.reply(":warning:Language Warning!!:warning:"+ /* Warn to sender */
+                                "\nPlease avoid using inappropriate language.").queue(
+                                    /* Report to the Server Owner */
+                                    (e)->{event.getJDA().getUserById(event.getGuild().getOwnerIdLong()).openPrivateChannel().queue( /* open owner DM */
+                                        (openPrivateChannel)->{openPrivateChannel.sendMessage( /* send Message in a format */
+                                                                chatMsg.getAuthor().getEffectiveName()+ // DiscordUser said: bla-bla-bla
+                                                                " said: "+ chatMsg.getContentDisplay()+ // on 20-02-2050  13:07:45
+                                                                "\non: "+chatMsg.getTimeCreated().      // Please have a look   
+                                                                            atZoneSameInstant(ZoneId.of("Asia/Kolkata")).
+                                                                            format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"))+
+                                                                "\n Please have a look."
+                                        ).queue(/* TODO: DEVELOP A BETTER SYSTEM TO MANAGE THE ABUSIVE RESPONSES */);
+                                        });
+                                });
+                    break;
+                }
             }
-            event.getChannel().sendMessage("Logging done.").queue();
-        }
+
+        });
+
         
     }
 
@@ -74,5 +105,8 @@ public class DiscordEventListener extends ListenerAdapter {
         
     }
 
+    @Override
+    public void onMessageUpdate(MessageUpdateEvent event){
 
+    }
 }
